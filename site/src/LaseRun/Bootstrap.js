7aa9caa786dbafbe4d30564b158141d5260bb616
Dorenavant <Ignatius@MaryvaleMBP15.local>
2014-06-09 12:39:48 -0400

    Looped music and I am not Dorenavant pls

--- site/src/LaseRun/Bootstrap.js
+++ site/src/LaseRun/Bootstrap.js
@@ -15,7 +15,7 @@ this.LaseRun = this.LaseRun || {};
         path.assets.level = path.getPath(LEVEL_DIR);
 
         var preload = function() {
-            this.load.audio("LaseRun/music", [path.assets.common.child("audio/music.mp3"), path.assets.common.child("audio/music.ogg")]);
+            this.load.audio("LaseRun/music", [path.assets.common.child("audio/music.mp3"), path.assets.common.child("audio/music.ogg")], true);
         }
 
         var create = function() {
@@ -24,7 +24,7 @@ this.LaseRun = this.LaseRun || {};
                 LaseRun.game.canvas.focus();
                 LaseRun.game.canvas.className = "automargin";
             }
-            LaseRun.music = this.add.audio("LaseRun/music");
+            LaseRun.music = this.add.audio("LaseRun/music", (LaseRun.settings.musicEnabled ^ true) ^ true, true);
             LaseRun.music.play();
             
             LaseRun.game.state.add("MainMenuState", new LaseRun.MainMenuState());
